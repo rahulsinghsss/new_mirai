31c78b631b2bb13408ddd7740cda2be1f1b7b9d4
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-09 11:36:23 +0530

    Update page.tsx

--- components/Amenities/Era_Interative_session/page.tsx
+++ components/Amenities/Era_Interative_session/page.tsx
@@ -123,7 +123,7 @@ const EraSeasons: React.FC = () => {
 
   const backgroundImages: { [key: string]: string } = {
     'bg-spring': 'https://d3p1hokpi6aqc3.cloudfront.net/AQUA%20POD_1.png',
-    'bg-summer': 'https://azure-baboon-302476.hostingersite.com/mirai_latest/media/pods/aviapod.png',
+    'bg-summer': 'https://d3p1hokpi6aqc3.cloudfront.net/Avia_Pod_1.png',
     'bg-autumn': 'https://d3p1hokpi6aqc3.cloudfront.net/Aminites_Tera_Pods_1.png',
     'bg-winter': 'https://azure-baboon-302476.hostingersite.com/mirai_latest/media/pods/pyro_pod.png',
     'bg-terra-pool': 'https://pavanimirai.com/55storeys-luxury-apartments-in-financial-district/media/infinitypool.jpg',
