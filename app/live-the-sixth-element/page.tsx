1cc8fc683b4cc1dd612e0573f3e4c54a734a2634
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-13 17:09:53 +0530

    Update page.tsx

--- app/live-the-sixth-element/page.tsx
+++ app/live-the-sixth-element/page.tsx
@@ -5,8 +5,7 @@ import Footer from '@/components/Life@Mirai/Footer/page'
 const page = () => {
   return (
     <>
-      {/* Main content wrapper - MUST have background and margin-bottom */}
-      <main className="relative z-10 bg-white pb-[100vh]">
+      <main className="relative z-10 bg-white mb-[100vh]">
         <PavaniMiraiBlog />
       </main>
       
