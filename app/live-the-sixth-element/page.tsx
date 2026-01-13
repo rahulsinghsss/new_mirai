3798c9d4ad78fad05fdf352ac65e61e220c176bd
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-13 16:41:26 +0530

    Update page.tsx

--- app/live-the-sixth-element/page.tsx
+++ app/live-the-sixth-element/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PavaniMiraiBlog from '@/components/live-the-sixth-element/page'
+import Footer from '@/components/Life@Mirai/Footer/page'
+
 
 const page = () => {
   return (
     <PavaniMiraiBlog />
+    <Footer />
   )
 }
 
 export default page
+
