1a544b3003578841568d6ad131f29df51ed7aad5
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-21 12:33:23 +0530

    Add Footer and main wrapper to page component
    Wrap CloudKissedRetreats component with main and add Footer.

--- app/cloud-kissed-retreats/page.tsx
+++ app/cloud-kissed-retreats/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Footer from '@/components/Life@Mirai/Footer/page'
 import CloudKissedRetreats from '@/components/cloud-kissed-retreats/page'
 const page = () => {
   return (
-    <CloudKissedRetreats />
+    <>
+      <main className="relative z-10 bg-white mb-[100vh]">
+        <CloudKissedRetreats  />
+      </main>
+      
+      <Footer />
+    </>
   )
 }
 
 export default page
+
