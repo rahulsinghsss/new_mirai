9d46e2551cfe8581ca38891d32646261038dc86d
Wingman-web <campaigns@wingmanbrandworks.com>
2026-01-22 10:35:29 +0530

    Update Sixthelementmobile.tsx

--- components/Home/Sixth_Element/Sixthelementmobile.tsx
+++ components/Home/Sixth_Element/Sixthelementmobile.tsx
@@ -99,7 +99,7 @@ export default function SixthElementMobile() {
             width: '100%',
             height: '100%',
             objectFit: 'cover',
-            objectPosition: 'center 20%',
+            objectPosition: 'center 40%',
             zIndex: 1,
           }}
         />
